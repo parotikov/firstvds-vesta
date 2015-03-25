a41872857a833dec8b36f71d40f01ba04bec4c12
parotikov <parotikov@users.noreply.github.com>
2015-03-25 17:03:44 +0300

    Update install_progs.sh

--- install_progs.sh
+++ install_progs.sh
@@ -33,3 +33,7 @@ v-change-user-package admin default
 
 # remove default.domain for admin
 v-delete-domain admin default.domain
+
+sed -i '/\$domain\/public_shtml/d' /usr/local/vesta/bin/v-add-web-domain
+sed -i '/\$domain\/private/d' /usr/local/vesta/bin/v-add-web-domain
+sed -i '/\$domain\/cgi-bin/d' /usr/local/vesta/bin/v-add-web-domain
